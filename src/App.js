51418285e5df24a30b44a820656c122f062a9a0a
naveensharma <sharmanaveen0010@gmail.com>
2023-05-11 12:31:48 +0530

    Add a photo logo

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import { useDispatch } from 'react-redux';
 import './App.css';
 import Navbar from './Navbar';
 import { Actioncreators } from './Actioncreators';
+import clothes from './img/clothes.jpeg';
 function App() {
   const dispatch= useDispatch()
   return (
     <>
     <Navbar/>
-    <h1 className='center'>The shoe store</h1>
+    <img src={clothes} alt='Network problem'/>
+    <h1 className='center'>The Cloth Store</h1>
     <div className="App">
      <button className='Increment btn' onClick={()=>dispatch(Actioncreators.Increment(100))}>Add</button>
-     Add the products of shoe
+     Add the products of Clothes
      <button className='Decrement btn' onClick={()=>dispatch(Actioncreators.Decrement(100))}>remove</button>
     </div>
     </>
